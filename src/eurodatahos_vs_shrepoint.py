3e69f357afe99a9857cc886c93a8d93f39caf501
Macktireh <abdimack97@gmail.com>
2021-12-14 23:47:44 +0100

    Organisation du code

--- src/eurodatahos_vs_shrepoint.py
+++ src/eurodatahos_vs_shrepoint.py
@@ -180,18 +180,18 @@ path_data_HOS = "E:/AppTotalEnergies/SRC-Python-comparaison/InputData/DataAppWee
 path_data_sharepoint = "E:/AppTotalEnergies/SRC-Python-comparaison/InputData/sharepoint.xlsx"
 path_Out = f"E:/AppTotalEnergies/SRC-Python-comparaison/OutputData/KPI-SIS-AFRIQUE-S{Week}-{today}.xlsx"
 
-# start = datetime.now()
-# m = EuroShare(path_data_HOS, path_data_sharepoint, path_Out)
-# m.reduce()
+start = datetime.now()
+m = EuroShare(path_data_HOS, path_data_sharepoint, path_Out)
+m.reduce()
 
-# os.system('cls' if os.name == 'nt' else 'clear')
-# print()
-# print("--------------------")
-# print("Terminer avec succès")
-# print("--------------------")
-# print()
+os.system('cls' if os.name == 'nt' else 'clear')
+print()
+print("--------------------")
+print("Terminer avec succès")
+print("--------------------")
+print()
 
-# end = datetime.now()
-# tm = end - start
-# print("temps d'exécution :", tm)
-# print()
+end = datetime.now()
+tm = end - start
+print("temps d'exécution :", tm)
+print()
